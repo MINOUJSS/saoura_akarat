820e93b4a8936b5b6c5dadc7100907845593a075
MINOUJSS <minoujss@gmail.com>
2022-01-28 19:38:24 +0100

    add template to customers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.index.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
